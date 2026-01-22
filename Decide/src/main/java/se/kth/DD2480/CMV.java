cd3cdc04f7b3b4d705db75947a5815dc2a2f39b2
Ahmed <ahmed.baccar@epfl.ch>
2026-01-22 12:17:46 +0100

    Revert "feat: Add LIC 0 (Closes #1)"
    This reverts commit 90c5d7039c612d6298e59edce6d3488c77db293d.

--- Decide/src/main/java/se/kth/DD2480/CMV.java
+++ Decide/src/main/java/se/kth/DD2480/CMV.java
@@ -1,15 +1,4 @@
 package se.kth.DD2480;
 
-
 public class CMV {
-    public CMV() {
-    }
-
-    boolean lic0(double length, Point[] points) {
-        int l = points.length;
-        for (int i = 0; i < l - 1; ++i) {
-            if (points[i].distance(points[i + 1]) > length) return true;
-        }
-        return false;
-    }
 }
--- Decide/src/main/java/se/kth/DD2480/Point.java
+++ Decide/src/main/java/se/kth/DD2480/Point.java
@@ -8,8 +8,4 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
-     double distance (Point p2){
-        return Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
-    }
 }
