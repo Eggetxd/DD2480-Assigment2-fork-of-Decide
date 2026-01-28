ad03acef123844babdf432e71fee267c19bb2cdc
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-29 00:02:51 +0100

    feat: Adding way of creating LCM's (#106)
    By calling the method createLCM with the input 0, 1 or 2, the user will
    get an LCM filled with values NOTUSED, ANDD or ORR.

--- src/main/java/se/kth/DD2480/LCM.java
+++ src/main/java/se/kth/DD2480/LCM.java
@@ -3,6 +3,7 @@ package se.kth.DD2480;
 public class LCM {
 
     CONNECTORS[][] arr;
+    static final int SIZE = 15;
 
     public LCM(CONNECTORS[][] arr) {
         this.arr = arr;
@@ -10,5 +11,31 @@ public class LCM {
 
     public CONNECTORS[][] getArr() {
         return arr;
+    }
+
+    private static CONNECTORS[][] filled(CONNECTORS c) {
+        CONNECTORS[][] m = new CONNECTORS[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                m[i][j] = c;
+            }
+        }
+
+        return m;
+    }
+
+    /**
+     * Method that creates a LCM depending on what input is given.
+     * @param mode, 0 = ALL NOTUSED, 1 = ALL ANDD, 2 = ALL ORR
+     * @return
+     */
+    public static LCM createLCM(int mode) {
+        return switch (mode) {
+            case 0 -> new LCM(filled(CONNECTORS.NOTUSED));
+            case 1 -> new LCM(filled(CONNECTORS.ANDD));
+            case 2 -> new LCM(filled(CONNECTORS.ORR));
+            default -> throw new IllegalArgumentException("Only allow whole numbers 0-2 as input");
+        };
     }
 }
