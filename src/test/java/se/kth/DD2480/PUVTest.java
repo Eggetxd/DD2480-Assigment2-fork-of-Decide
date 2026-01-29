56d5a89f91789bdb2aa212b0ac9baa8d35c69e92
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-29 01:55:55 +0100

    feat: Adding tests for PUV (#110)
    Somehow commited space in last commit. Here are the tests for PUV.
    We test the two input value for the createPUV method.

--- src/test/java/se/kth/DD2480/PUVTest.java
+++ src/test/java/se/kth/DD2480/PUVTest.java
@@ -2,6 +2,7 @@ package se.kth.DD2480;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +16,19 @@ class PUVTest {
     void tearDown() {
     }
 
+    @Test
+    void creatPUVwith0isFilledWithFalse() {
+        PUV puv = PUV.createPUV(0);
+        for (int i = 0; i < 15; i++) {
+            assertFalse(puv.getArray()[i]);
+        }
+    }
 
+    @Test
+    void creatPUVwith1isFilledWithTrue() {
+        PUV puv = PUV.createPUV(1);
+        for (int i = 0; i < 15; i++) {
+            assertTrue(puv.getArray()[i]);
+        }
+    }
 }
