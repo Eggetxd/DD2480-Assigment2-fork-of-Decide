07e906c5b53622051303dd6e0ad68cbaca99333b
Adam Egnell <71374893+Eggetxd@users.noreply.github.com>
2026-02-12 05:03:09 +0100

    Add assertion for null points in CMVTest

--- src/test/java/se/kth/DD2480/CMVTest.java
+++ src/test/java/se/kth/DD2480/CMVTest.java
@@ -32,6 +32,7 @@ class CMVTest {
             cmv.lic0(null, 1.0, 2);
         });
         assertEquals("'points' must not be null", error.getMessage());
+        assertTrue(false);
     }
 
     /**
